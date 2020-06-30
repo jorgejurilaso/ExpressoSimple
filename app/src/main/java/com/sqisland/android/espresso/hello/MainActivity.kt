c3be250e94eec668215a3709e4bd95ba8ebf1332
jjuri <jorge.juri@hotmail.com>
2020-06-30 15:08:47 -0700

    Recompilar

--- app/src/main/java/com/sqisland/android/espresso/hello/MainActivity.kt
+++ app/src/main/java/com/sqisland/android/espresso/hello/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
   private lateinit var greetButton: View
@@ -11,6 +14,8 @@ class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    AppCenter.start(application, "a3887cf8-1d74-48c2-9d6e-063ffbb858ab",
+            Analytics::class.java, Crashes::class.java)
     setContentView(R.layout.activity_main)
     greetButton = findViewById(R.id.greet_button)
     greetingView = findViewById(R.id.greeting)
